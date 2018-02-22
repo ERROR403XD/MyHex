12870fcfc0544e33eaf6b1af4c8e3fcd1973b72f
ERROR403XD <30397081+ERROR403XD@users.noreply.github.com>
2018-02-22 20:25:59 +0800

    hex

---
+++ MyHex/MyHex/Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHex
+{
+    class Board
+    {
+        private List<BoardHex> hexList;
+        public Board()
+        {
+
+        }
+    }
+}
---
+++ MyHex/MyHex/Hex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHex
+{
+    class Hex
+    {
+        public Hex[] neighbor; 
+        public Hex()
+        {
+            neighbor = new Hex[6];
+            for(int i = 0;i<6;i++)
+            {
+                neighbor[i] = null;
+            }
+        }
+    }
+    class BoardHex:Hex
+    {
+        private bool filled;
+        public bool Filled
+        {
+            get
+            {
+                return filled;
+            }
+        }
+        public BoardHex()
+        {
+            neighbor = new BoardHex[6];
+            for(int i = 0;i<6;i++)
+            {
+                neighbor[i] = null;
+            }
+            filled = false;
+        }
+    }
+}
